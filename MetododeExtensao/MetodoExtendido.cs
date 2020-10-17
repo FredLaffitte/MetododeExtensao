1e676f0a188f2114a5f6815437465f9d685d7ad2
Frederico Laffitte <frederico.laffitte@supergasbrascombr.onmicrosoft.com>
2020-10-17 18:28:43 -0300

    Adicionar arquivos de projeto.

---
+++ MetododeExtensao/MetodoExtendido.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MetododeExtensao
+{
+    static class MetodoExtendido
+
+    {
+
+        ///
+        /// Método que trocara letras Maiusculas por minusculas
+        ///
+        ///
+        ///
+
+        public static String InverteCase(this string str)
+        {
+            //variavel para guardar o valor temporario
+            string temp = "";
+            //percorro a lista de caracteres no texto str
+            foreach (char ch in str)
+            {
+                //realizo a inversão de maiuscula e minuscula
+                if (Char.IsLower(ch))
+                    temp += Char.ToUpper(ch);
+                else
+                    temp += Char.ToLower(ch);
+            }
+            //retorno
+            return temp;
+        }
+    }
+
+}
---
+++ MetododeExtensao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MetododeExtensao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto = "Looke A MelhOR plataforma de strEAMing de FILMES do Mundo.";
+
+            Console.WriteLine("===========================================================");
+            Console.WriteLine("=Alterando as letras maiúsculas e minúsculas de uma string=");
+            Console.WriteLine("===========================================================");
+            Console.WriteLine($"Antes de inverter..: {texto}");
+            Console.WriteLine($"Depois de invertido: {texto.InverteCase()}");
+            Console.ReadLine();
+        }
+    }
+}
